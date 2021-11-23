9c631c926efe04c774b941ead3e1ee38ad2822ca
loch <nigelloch@gmail.com>
2021-11-23 16:45:48 -0600

    added delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 ///////////////////////////////////////
 var taskFormHandler = function(event) {
@@ -56,7 +57,8 @@ var createTaskEl = function(taskDataObj) {
 
   }
 
-  var createTaskActions = function(taskId) {
+/////////////////////////////////////////////
+var createTaskActions = function(taskId) {
       var actionContainerEl = document.createElement("div");
       actionContainerEl.className = "task-actions";
     // create edit button
@@ -100,3 +102,19 @@ var createTaskEl = function(taskDataObj) {
 //////////////////////////////////////////
 formEl.addEventListener("submit", taskFormHandler);
 
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the elements task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+}
+    var deleteTask = function(taskId) {
+        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+        taskSelected.remove();
+};
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
